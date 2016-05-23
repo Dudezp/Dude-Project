2f9a2e7dd2de610aa2690c68fbec178d03ba84e4
Dude_zp <alex_dude@i.ua>
2016-05-23 21:11:11 +0300

    Lesson JS

---
+++ JS/Lesson.js
@@ -0,0 +1,7 @@
+/**
+ * Created by dude_ on 23.05.2016.
+ */
+var name = prompt("Item call:");
+var pricePerItem = prompt("Get price of item");
+var count = prompt("Get count of items:");
+alert("Thank you for buying " + name + "(s). " + "Total:" + pricePerItem*count);
